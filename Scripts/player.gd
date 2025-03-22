1854c616398a3a07e76435599628bc4b7cedc12f
xXBunchXx <151859630+xXBunchXx@users.noreply.github.com>
2025-03-22 18:22:54 +0000

    22/03/2025 (18:21)
    - Added globals
    - Added jump and fall animation
    - Code needs a little tidying up

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
+@onready var animation = $AnimatedSprite2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
+var wasInAir: bool = false
+
+const speed = 150.0
+const jumpVelocity = -300.0
 
 
 func _physics_process(delta: float) -> void:
+	print(wasInAir)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if not wasInAir:
+			animation.play("jumpTakeOff")
+	# Checks if you have just landed
+		wasInAir = true
+		
+	if is_on_floor():
+		if wasInAir:
+			on_land()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		#animation.play("jumpTakeOff")
+		velocity.y = jumpVelocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+# This carries out whenever you land
+func on_land():
+	animation.play("jumpLanding")
+	wasInAir = false
