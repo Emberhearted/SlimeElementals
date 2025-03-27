a3ddfd6b94ff98c76166bc716cabdb93c682833e
xXBunchXx <151859630+xXBunchXx@users.noreply.github.com>
2025-03-27 10:43:53 +0000

    27/03/25 (10:42)
    Implemented jump on release with simple build up mechanic, still needs to have timers for if held too long it cancels the jump etc

--- Scripts/player_v_2.gd
+++ Scripts/player_v_2.gd
@@ -28,13 +28,17 @@ func _physics_process(delta: float) -> void:
 		velocity.y += global.gravity * delta
 	
 	# Handle jump left
-	if Input.is_action_just_pressed("jump") and Input.is_action_pressed("left") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and velocity.x <= 0:
+		animation.play("JumpLeftBuildUp")
+	if Input.is_action_just_released("jump") and is_on_floor():
 		velocity.y = jumpVelocity
 		prev_direction = -1
 		animation.play("JumpLeft")
 
 	# Handle jump right
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and velocity.x > 0:
+		animation.play("JumpRightBuildUp")
+	if Input.is_action_just_released("jump") and is_on_floor():
 		velocity.y = jumpVelocity
 		prev_direction = 1
 		animation.play("JumpRight")
@@ -68,5 +72,6 @@ func _physics_process(delta: float) -> void:
 			on_land_left()
 		else:
 			on_land_right()
+	
 	
 	was_in_air = not is_on_floor()
